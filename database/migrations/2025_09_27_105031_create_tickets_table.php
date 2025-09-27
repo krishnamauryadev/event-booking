70384b1b521c2779bf6731b527b57b19efec716a
Krishna Maurya <krishna@iconnectgroup.com>
2025-09-27 16:22:10 +0530

    fix migrations

--- database/migrations/2025_09_27_105031_create_tickets_table.php
+++ database/migrations/2025_09_27_105031_create_tickets_table.php
@@ -1,15 +1,17 @@
-
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
-        Schema::create('tickets', function (Blueprint $table) {
+         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->string('type');
             $table->decimal('price', 10, 2);
@@ -19,8 +21,11 @@ class CreateTicketsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('tickets');
     }
-}
+};
--- database/migrations/2025_09_27_105133_create_payments_table.php
+++ database/migrations/2025_09_27_105133_create_payments_table.php
@@ -1,15 +1,17 @@
-
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
-        Schema::create('payments', function (Blueprint $table) {
+         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained('bookings')->onDelete('cascade');
             $table->decimal('amount', 10, 2);
@@ -18,8 +20,11 @@ class CreatePaymentsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('payments');
     }
-}
+};
